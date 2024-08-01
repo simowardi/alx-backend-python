ef891534b275c37b3beb4ba7a0c8a957fbbf6286
simowardi <daoist32@gmail.com>
2024-08-01 17:17:36 +0000

    Python Variable annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
-"""
-Defines a function that adds two floats and returns their sum.
-
-Args:
-    a (float): The first float to be added.
-    b (float): The second float to be added.
-
-Returns:
-    float: The sum of the two input floats.
-"""
+!/usr/bin/env python3
 
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floats.
+
+    Args:
+        a (float): The first float to add.
+        b (float): The second float to add.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""
-Defines a function that concatenates two strings.
-
-Args:
-    str1 (str): The first string to be concatenated.
-    str2 (str): The second string to be concatenated.
-
-Returns:
-    str: The concatenation of the two input strings.
-"""
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string to concatenate.
+        str2 (str): The second string to concatenate.
+
+    Returns:
+        str: The concatenation of str1 and str2.
+    """
     return str1 + str2
